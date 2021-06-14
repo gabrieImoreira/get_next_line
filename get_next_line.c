2eeb13fbd93baeb352f2465210b665ed071cb00d
gams <gabriel.antonioms@gmail.com>
2021-06-14 19:06:37 -0300

    initial tests

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: gantonio <gantonio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/09 23:17:45 by gantonio          #+#    #+#             */
-/*   Updated: 2021/06/14 16:25:23 by gantonio         ###   ########.fr       */
+/*   Updated: 2021/06/14 19:02:49 by gantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,11 @@
 
 int get_next_line(int fd, char **line)
 {
-	  
+	int ret;
+	char buf;
+	
+	buf = **line;
+	ret = read(fd, buf, BUF_SIZE);
+	ft_putstr_fd(buf, 1);
+
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,6 +1,10 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+#ifndef BUF_SIZE
+#define BUF_SIZE 10
+#endif
+
 # include <unistd.h>
 # include <stdlib.h>
 
