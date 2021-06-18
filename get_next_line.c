893d5bc84fbb3125bd4fb3c089b04e8a36b851e2
gams <gabriel.antonioms@gmail.com>
2021-06-17 22:19:22 -0300

    add initial version 5

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,12 @@
 /*   By: gantonio <gantonio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/09 23:17:45 by gantonio          #+#    #+#             */
-/*   Updated: 2021/06/17 19:56:49 by gantonio         ###   ########.fr       */
+/*   Updated: 2021/06/17 22:18:40 by gantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
+#include <stdio.h>
 
 void	ft_bzero(void *str, size_t len)
 {
@@ -22,46 +22,54 @@ void	ft_bzero(void *str, size_t len)
 		*ptr++ = '\0';
 }
 
-// static char *ft_return_new_line(void *str)
-// {
-	
-// }
-
-int get_next_line(int fd, char **line)
+static char	*ft_return_new_line(int fd, char *buf, char *repository, int *ret)
 {
-	int ret;
-	char *buf;
-	size_t len;
-	char *new_line;
-	char *temp;
-	static char *repository;
-	int i;
-	
-	len = -1;
-	i = 0;
-	buf = malloc(sizeof(char) * BUFFER_SIZE + 1);
-	ret = read(fd, buf, BUFFER_SIZE);
-	new_line = malloc(sizeof(char) * BUFFER_SIZE + 1);
-	temp = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	int		is_new_line;
+	size_t	len;
+	int		bytes_read;
+	char	*new_line;
+	char	*temp;
+	int		i;
 
-	if (!repository)
-		repository = malloc(sizeof(char) * BUFFER_SIZE + 1);
-	repository = ft_strjoin(repository, buf);
-	
-	if(ft_strchr(repository, '\n')){
-		while (repository[++len] != '\n' && len <= BUFFER_SIZE)
-	 		new_line[len] = repository[len];
+	i = 0;
+	is_new_line = 1;
+	len = -1;
+	temp = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	new_line = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	while (is_new_line)
+	{
+		bytes_read = read(fd, buf, BUFFER_SIZE);
+		repository = ft_strjoin(repository, buf);
+		if (ft_strchr(repository, '\n') || bytes_read <= 0)
+			is_new_line = 0;
 	}
-	//printf("nl:%s\n\n", new_line);
-	while (repository[len++])
-		temp[i++] = repository[len];
+	while (repository[++len] != '\n' && repository[len])
+		new_line[len] = repository[len];
+	new_line[len] = '\0';
+	while (repository[++len])
+	{
+		temp[i] = repository[len];
+		i++;
+	}
 	ft_bzero(repository, ft_strlen(repository));
 	repository = ft_strjoin(repository, temp);
-	
-	//printf("\n\nrp:%s\n", repository);
-	*line = new_line;
-	//*line = repository;
-	//*line = temp;
+	free(temp);
+	*ret = bytes_read;
+	return (new_line);
+}
 
-	return(ret);
+int	get_next_line(int fd, char **line)
+{
+	char		*buf;
+	char		*new_line;
+	int			*ret;
+	static char	*repository;
+
+	ret = malloc(sizeof(int));
+	buf = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	new_line = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (!repository)
+		repository = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	*line = ft_return_new_line(fd, buf, repository, ret);
+	return (1);
 }
