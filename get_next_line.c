3a656471fec28413eb5c8a763946049672ed2f9b
gams <gabriel.antonioms@gmail.com>
2021-06-19 00:36:36 -0300

    initial version 6(return string if value is greater than line

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: gantonio <gantonio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/09 23:17:45 by gantonio          #+#    #+#             */
-/*   Updated: 2021/06/18 00:19:01 by gantonio         ###   ########.fr       */
+/*   Updated: 2021/06/19 00:34:45 by gantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,32 +25,43 @@ void	ft_bzero(void *str, size_t len)
 static char	*ft_return_new_line(int fd, char *buf, char *repository, int *ret)
 {
 	int		is_new_line;
-	size_t	len;
+	int		len;
 	int		bytes_read;
 	char	*new_line;
 	char	*temp;
 	int		i;
 
 	i = 0;
-	is_new_line = 1;
 	len = -1;
+	is_new_line = 1;
 	temp = malloc(sizeof(char) * BUFFER_SIZE + 1);
 	new_line = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if(!new_line)
+		return (NULL);
 	while (is_new_line)
 	{
 		bytes_read = read(fd, buf, BUFFER_SIZE);
-		repository = ft_strjoin(repository, buf);
+		//printf("\nstrlcpy -> rep init: |%s|", repository);
+		ft_strlcat(repository, buf, ft_strlen(buf) + ft_strlen(repository) + 1);
+		//printf("\nstrlcpy -> rep end: |%s|", repository);
 		if (ft_strchr(repository, '\n') || bytes_read <= 0)
-			is_new_line = 0;
+			break;
 	}
+	//printf("\nrep FORA: |%s|", repository);
+	//printf("%d", len);
 	while (repository[++len] != '\n' && repository[len])
+	{
+		printf(",%c", new_line[len]);
 		new_line[len] = repository[len];
+	}
 	new_line[len] = '\0';
+	printf("\nnew line: |%s|\n", new_line);
 	while (repository[++len])
 		temp[i++] = repository[len];
 	ft_bzero(repository, ft_strlen(repository));
-	repository = ft_strjoin(repository, temp);
+	ft_strlcat(repository, temp, ft_strlen(repository) + ft_strlen(temp) + 1);
 	free(temp);
+	//printf("\nrep end: %s\n\n", repository);
 	*ret = bytes_read;
 	return (new_line);
 }
@@ -67,5 +78,10 @@ int	get_next_line(int fd, char **line)
 	if (!repository)
 		repository = malloc(sizeof(char) * BUFFER_SIZE + 1);
 	*line = ft_return_new_line(fd, buf, repository, ret);
-	return (1);
+	if(ret > 0)
+		return (1);
+	else if(ret < 0)
+		return (-1);
+	else
+		return (0);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: gantonio <gantonio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/16 20:54:10 by gantonio          #+#    #+#             */
-/*   Updated: 2021/06/17 19:15:54 by gantonio         ###   ########.fr       */
+/*   Updated: 2021/06/19 00:34:41 by gantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,36 +57,40 @@ size_t	ft_strlen(const char *str)
 int	ft_strchr(const char *str, int c)
 {
 	size_t	i;
-
+	
 	i = 0;
-	while (1)
+	while (str[i])
 	{
 		if (str[i] == (unsigned char)c)
 			return (1);
-		if (!str[i])
-			return (0);
 		i++;
 	}
+	return (0);
 }
 
-char	*ft_strjoin(char const *s1, char const *s2)
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
-	char	*ptr;
-	size_t	i;
-	size_t	c;
+	unsigned int	d;
+	unsigned int	j;
+	unsigned int	i;
+	unsigned int	s;
 
 	i = 0;
-	c = 0;
-	ptr = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
-	if (!ptr || !s1 || !s2)
-		return (NULL);
-	while (s1[i])
+	s = ft_strlen(src);
+	d = ft_strlen(dest);
+	j = s + d;
+	if (size > d)
 	{
-		ptr[i] = s1[i];
-		i++;
+		while (d + 1 < size && src[i] != '\0')
+		{
+			dest[d] = src[i];
+			i++;
+			d++;
+		}
+		dest[d] = '\0';
+		return (j);
 	}
-	while (s2[c])
-		ptr[i++] = s2[c++];
-	ptr[i] = '\0';
-	return (ptr);
+	if (size == d)
+		return (ft_strlen(src) + size);
+	return (ft_strlen(src) + size);
 }
